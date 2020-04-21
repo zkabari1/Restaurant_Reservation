c7153295056defc3ec97db13597ccc464f6c09f3
zimple kabariya <zkabari1@binghamton.edu>
2020-04-20 21:31:23 -0400

    added code coverage using istanbul

--- test/unit/lib/reservations.js
+++ test/unit/lib/reservations.js
@@ -1,11 +1,26 @@
+const proxyquire = require("proxyquire");
+const sinon = require("sinon");
 const chai = require("chai");
 const should = chai.should();
+const sinonChai = require("sinon-chai");
 const Reservation = require("../../../lib/schema/reservation");
-const reservations = require("../../../lib/reservations");
+const db = require("sqlite");
+chai.use(sinonChai);
 
-describe("Reservation Library", function () {
+describe("Reservations Library", function () {
+  const debugStub = function () {
+    return sinon.stub();
+  };
+  let reservations;
+
+  before(function () {
+    reservations = proxyquire("../../../lib/reservations", {
+      debug: debugStub,
+    });
+  });
+
   context("Validate", function () {
-    it("it should pass a valid reservation with no optional fields", function () {
+    it("should pass a valid reservation with no optional fields", function () {
       const reservation = new Reservation({
         date: "2017/06/10",
         time: "06:02 AM",
@@ -18,7 +33,8 @@ describe("Reservation Library", function () {
         .validate(reservation)
         .then((actual) => actual.should.deep.equal(reservation));
     });
-    it("should fail a reservation with bad email", function () {
+
+    it("should fail an invalid reservation with a bad email", function () {
       const reservation = new Reservation({
         date: "2017/06/10",
         time: "06:02 AM",
@@ -26,9 +42,81 @@ describe("Reservation Library", function () {
         name: "Family",
         email: "username",
       });
+
       return reservations
         .validate(reservation)
         .catch((error) => error.should.be.an("error").and.not.be.null);
+    });
+  });
+
+  context("Create", function () {
+    let dbStub;
+
+    before(function () {
+      dbStub = sinon.stub(db, "run").resolves({
+        stmt: {
+          lastID: 1349,
+        },
+      });
+      reservations = proxyquire("../../../lib/reservations", {
+        debug: debugStub,
+        sqlite: dbStub,
+      });
+    });
+
+    after(function () {
+      dbStub.restore();
+    });
+
+    it("should return the created reservation ID", function (done) {
+      const reservation = new Reservation({
+        date: "2017/06/10",
+        time: "06:02 AM",
+        party: 4,
+        name: "Family",
+        email: "username@example.com",
+      });
+
+      reservations
+        .create(reservation)
+        .then((lastID) => {
+          lastID.should.deep.equal(1349);
+          done();
+        })
+        .catch((error) => done(error));
+    });
+  });
+  context("Save", function () {
+    let dbMock;
+
+    before(function () {
+      dbMock = sinon.mock(db);
+    });
+
+    after(function () {
+      dbMock.restore();
+    });
+
+    it("should only call the database once", function () {
+      dbMock.expects("run").once();
+
+      reservations = proxyquire("../../../lib/reservations", {
+        debug: debugStub,
+        sqlite: dbMock,
+      });
+
+      const reservation = {
+        datetime: "2017-06-10T06:02:00.000Z",
+        party: 4,
+        name: "Family",
+        email: "username@example.com",
+        message: undefined,
+        phone: undefined,
+      };
+
+      reservations.save(reservation);
+
+      dbMock.verify();
     });
   });
 });
